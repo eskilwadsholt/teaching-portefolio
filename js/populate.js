e936af78c8fb33c907206012c056dfddccb6e67a
Eskil Simon Kanne Wadsholt <eskilsimonkannewadsholt@gmail.com>
2020-05-10 20:37:49 +0200

    updated completely

--- js/populate.js
+++ js/populate.js
@@ -1,14 +1,70 @@
+var $seasons = {};
+var $seasonKeys = [];
+
 $.getJSON( "../data/courses.json", function( data ) {
     $.each(data, function(_, element) {
         var items = [];
-        $.each(element, function(key, val) {
-            console.log(key);
-            console.log(val);
-            items.push( `<li>${key}: ${val}</li>` );
-        });
-        $( "<ul/>", {
-            "class": "course",
-            html: items.join("\n")
-        }).appendTo(".rows");
+        var season = element.start + "/" + (element.start + 1);
+        if (season in $seasons) {
+            $seasons[season].push(element);
+        }
+        else {
+            $seasons[season] = [ element ];
+            $seasonKeys.push(season);
+        }
+    });
+    $.each($seasonKeys, function(i, key) {
+        var val = $seasons[key];
+        var HTML = `<div class="season">${seasonDiv(key)} ${seasonContent(val)}</div>`;
+        $(HTML).prependTo(".rows");
     });
 });
+
+function seasonDiv(season) {
+    return `
+    <div class="season__title">
+        ${season}
+    </div>`;
+}
+
+function seasonContent(content) {
+    return `
+    <div class="season__content">
+        ${formatEntries(content)}
+    </div>`;
+}
+
+function formatEntries(entries) {
+    var result = "";
+    entries.forEach(entry => result += formatEntry(entry));
+    return result;
+}
+
+function formatEntry(entry) {
+    return `
+    <div class="entry">
+        <div class="${entry.branch}">
+            ${entry.branch}
+        </div>
+        <div class="subject-and-level">
+            <div class="subject">
+                ${entry.subject}
+            </div>
+            <div class="level">
+                ${entry.level}
+            </div>
+        </div>
+        <div class="participants">
+            ${entry.participants} elev${entry.participants == 1 ? "" : "er"}
+        </div>
+        <div class="content">
+            &bullet; ${entry.content}
+        </div>
+        <div class="comment">
+            ${entry.comment ? "#" + entry.comment : ""}
+        </div>
+    </div>
+    `;
+}
+
+$("#birthday").html("23.11.1979");
